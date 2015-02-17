089479a58548ec8853a1cf6926b7fb53a99a17aa
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2015-02-17 00:03:47 -0500

    DocumentStore functions for file/tag creation
    - Creating a DocumentDataStorage allows one to add files and tag them
      via name and persist this to a mongodb database underneath
    
    - Need to create selection methods to expose the database to the outside
      world. (Right now it's technically public but I'll change that)
    
    Notes:
    
    There has got to be a better way of dealing with Option[Type] because
    treating it as a collection and hitting the .foreach machine seems
    really bizarre and kind of silly to me. I'm probably missing something
    really simple.

--- src/main/scala/com/example/sandbox.scala
+++ src/main/scala/com/example/sandbox.scala
@@ -8,6 +8,33 @@ class DocumentDataStorage(xmlConfFilePath: String, environment: String = "local"
 	assert(datasource.length == 1)
 
 	val mongoClient = MongoClient( (datasource \ "host").text , (datasource \ "port").text.toInt)
+	val db = mongoClient("filetagger")
+	val collection = db("filetagger")
+	
+
+	def addFile(fileName: String) {
+		collection.insert(MongoDBObject("name" -> fileName, "tags" -> List[String]()))
+	}
+
+	def addTagToFile(fileName: String, tag: String) : Boolean = {
+		var resultOfOperation : Boolean = false
+
+		//This foreach method on the findOne seems wrong, i must be missing something 
+		//with options
+		collection.findOne(MongoDBObject("name" -> fileName)).foreach { c =>
+			c.getAs[List[String]]("tags").foreach { tags => 
+					if (!tags.contains(tag)) {
+						val tagList = tags ++ tag
+						val update = $set("tags" -> tagList)
+						val result = collection.update(c, update, upsert=true)
+						assert(result.getN == 1)
+						resultOfOperation = true
+					}		
+				}
+			}
+		return resultOfOperation  
+	}
+
 
 	
 }
